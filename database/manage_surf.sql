78d87ccdfee3fe8041edd689d41c3eddb336fc6a
hassan_aftah <aftahhassan25@gmail.com>
2026-03-31 15:53:15 +0000

    commit

--- database/manage_surf.sql
+++ database/manage_surf.sql
@@ -61,7 +61,7 @@ CREATE TABLE `students` (
   `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
+-- ----------------------------------------------------------
 
 --
 -- Structure de la table `users`
